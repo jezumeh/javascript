711d90f108e9e5cbed54539a738eec0c411732a1
jezumeh <jesus.j.vela.jr@outlook.com>
2025-03-16 23:28:44 -0500

    Refined quiz.js and .html

--- quiz.js
+++ quiz.js
@@ -4,69 +4,47 @@ Assignment: Functions
 -JJVJ
 */
 
-//Array Declarations
+//Array and Variable Declarations
 var question = [
-    '<h2>What is the capital of Texas?</h2>', //Austin
-    
-    '<h2>What is the capital of Georgia?</h2>', //Atlanta
-
-    '<h2>What is the capital of Colorado?</h2>', //Denver
+    'What is the capital of Texas?', //Austin
+    'What is the capital of Georgia?', //Atlanta
+    'What is the capital of Colorado?' //Denver
 ];
 var answer = [
     'Austin', //Texas
     'Atlanta', //Georgia
-    'Denver', //Colorado
+    'Denver' //Colorado
 ];
+var aL = [' trys left)', ' try left)']; // user feedback in prompts (i.e. "3 trys left")
 
-document.getElementById("quizHtml").innerHTML = "Start the Quiz";
+function stateQuiz(){
+    var points = 0;
 
-function quiz (){
-    var guess = 3; //"Trys" on a Question
-    var score;
-    var totalPoints = 0 + subPoints;
+    alert('STATE CAPITAL QUIZ\r\rCheck if you know the names of three states\' capital cities. Make sure you spell each guess correctly and capitalize it.\r\rGood luck!');
 
-    round = prompt(question[0]); 
-    if (round == answer[0]){
-        score += 1;
-        alert('You are right!')
-    } else{
-        alert('You are wrong!')
+    for (var i = 0; i <= 2; i++){
+        var attempt = 3;
+        var b = i; // Numbers each prompt (i.e. "1. Question...?"")
+
+        while (attempt > 0){ //round of a question
+
+            if (attempt > 1){ //further customizes user feedback
+                n = 0;
+            } else{
+                n = 1;
+            }
+
+            promptQuestion = prompt((b+=1) + '. ' + question[i] + ' (' + attempt + aL[n]); //question prompt
+            
+            if (promptQuestion == answer[i]){ //check arrays
+                points += attempt;
+                alert('That is right! Points: ' + points);
+                attempt = 0;
+            } else{
+                alert('That is wrong.');
+                attempt -= 1;
+            }
+        }
     }
-
-
+    document.write('<h2 class="hdg">' + 'Final Score: ' + points + '/9 Points!' + '</h2>'); //final score appears on front end
 }
-
-// set up question and matching answer arrays
-var mathQuest = ['3 + 2', '7 - 4', '10 / 5', '6 X 2'];
-var mathAns = ['5', '3', '2', '12'];
-
-// math quiz function displays math questions and gives the user 2 attempts to get it right - then returns a score
-function mathQuiz() {
-  // initialize points
-  var points = 0;
-  //use a for loop to loop through each math question
-  for (i = 0; i <= 3; i++) {
-    //set attempts to 3
-    var attempts = 3;
-    // use a while loop to give the user 3 attempts to answer
-    while (attempts > 0) {
-      // prompt user with math question
-      ans = prompt("What is " + mathQuest[i]);
-      // see if the answer matches the answer array
-      if (ans == mathAns[i]) {
-        // if correct, add 1 to points, alert user and set attempts to 0
-        points = points + 1;
-        alert("Correct!");
-        attempts = 0;
-      } else {
-        // if answer doesn't match, alert user and subtract 1 from attempts
-        alert("Incorrect");
-        attempts = attempts - 1;
-      } // end if
-    } // end while
-  } // end for
-  // return the points variable
-  return points;
-} // end mathQuiz function
-
-
